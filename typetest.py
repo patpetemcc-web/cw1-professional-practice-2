94fb8dc9981b8d8595d9b107425addb148b21447
jmo6345 <dud3xo@gmail.com>
2026-03-24 10:37:01 +0000

    Added typetest.py and wordbank.json

---
+++ typetest.py
@@ -0,0 +1,29 @@
+import json
+import random
+
+def load_bank(filename="wordbank.json"):
+    with open(filename, "r") as f:
+        return json.load(f)
+    
+class SentenceLoader:
+    def __init__(self, word_bank, difficulty="Easy"):
+        self.word_bank = word_bank
+        self.difficulty = difficulty
+        self.used = set()
+    
+    def get_sentence(self):
+        available = list(set(self.word_bank[self.difficulty]) - self.used)
+
+        if not available:
+            self.used.clear()
+            available = self.word_bank[self.difficulty]
+
+        sentence = random.choice(available)
+        self.used.add(sentence)
+        return sentence
+
+word_bank = load_bank("wordbank.json")
+manager = SentenceLoader(word_bank, "Easy")
+print("Testing random sentence output:\n")
+for i in range(1):
+    print(manager.get_sentence())
